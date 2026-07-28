a5f7e44610e424bc82602fdd5459bf7322b4f422
lolguy118 <146142050+lolguy118@users.noreply.github.com>
2026-07-28 14:38:24 +0000

    feat(Timer8): finished abstraction of Timer8

--- lib/Timer8.cpp
+++ lib/Timer8.cpp
@@ -1,13 +1,18 @@
 #include "Timer8.h"
-#include <stdint.h>
 #include <assert.h>
 
 Timer8::Timer8(UniversalTimerRegisters argUniversalTimerRegisters, Timer8SpecificRegisters argTimer8Registers) : mUniversalRegisters(argUniversalTimerRegisters), mTimer8SpecificRegisters(argTimer8Registers) {}
 
+bool Timer8::isRunning()
+{
+    uint8_t temp = mUniversalRegisters.tccrb.read();
+    return (temp & 0b00000111) != 0;
+}
+
 void Timer8::setPrescaler(Prescaler argPrescalar)
 {
     mPrescaler = argPrescalar;
-    if (mIsOn)
+    if (isRunning())
     {
         writePrescaler();
     }
@@ -35,7 +40,7 @@ void Timer8::writePrescaler()
         temp |= 0b00000111;
         break;
     default:
-        assert(mPrescaler == Prescaler::DIV_1 && mPrescaler == Prescaler::DIV_8 && mPrescaler == Prescaler::DIV_64 && mPrescaler == Prescaler::DIV_256 && mPrescaler == Prescaler::DIV_1024);
+        assert(mPrescaler == Prescaler::DIV_1 || mPrescaler == Prescaler::DIV_8 || mPrescaler == Prescaler::DIV_64 || mPrescaler == Prescaler::DIV_256 || mPrescaler == Prescaler::DIV_1024);
         break;
     }
     mUniversalRegisters.tccrb.write(temp);
@@ -64,37 +69,25 @@ void Timer8::setMode(Mode argMode)
         break;
     case Mode::PHASE_CORRECT_PWM_COMPARE_A_TOP:
         enablePhaseCorrectPWMCompareATop();
+        break;
     default:
-        assert(mMode == Mode::NORMAL && mMode == Mode::CTC && mMode == Mode::FAST_PWM && mMode == Mode::PHASE_CORRECT_PWM);
+        assert(mMode == Mode::NORMAL || mMode == Mode::CTC || mMode == Mode::FAST_PWM || mMode == Mode::PHASE_CORRECT_PWM);
         break;
     }
 }
 
-void Timer8::setCompareValueA(int argCompareValue)
+void Timer8::writeCompareValueA(uint8_t argCompareValue)
 {
-    mCompareValueA = argCompareValue;
-    writeCompareValueA();
+    mTimer8SpecificRegisters.ocra.write(argCompareValue);
 }
 
-void Timer8::setCompareValueB(int argCompareValue)
+void Timer8::writeCompareValueB(uint8_t argCompareValue)
 {
-    mCompareValueB = argCompareValue;
-    writeCompareValueB();
-}
-
-void Timer8::writeCompareValueA()
-{
-    mTimer8SpecificRegisters.ocra->write(mCompareValueA);
-}
-
-void Timer8::writeCompareValueB()
-{
-    mTimer8SpecificRegisters.ocrb->write(mCompareValueB);
+    mTimer8SpecificRegisters.ocrb.write(argCompareValue);
 }
 
 void Timer8::start()
 {
-    mIsOn = true;
     writePrescaler();
 }
 
@@ -157,3 +150,76 @@ void Timer8::enablePhaseCorrectPWMCompareATop()
     temp |= (1 << 3);
     mUniversalRegisters.tccrb.write(temp);
 }
+
+void Timer8::setChannelAMode(OutputCompareMode argChannelAMode)
+{
+    uint8_t temp = mUniversalRegisters.tccra.read();
+    temp &= ~(0b11 << 6);
+    switch (argChannelAMode)
+    {
+    case OutputCompareMode::DISCONNECTED:
+        break;
+    case OutputCompareMode::TOGGLE:
+        temp |= (1 << 6);
+        break;
+    case OutputCompareMode::NON_INVERTING_PWM:
+        temp |= (1 << 7);
+        break;
+    case OutputCompareMode::INVERTING_PWM:
+        temp |= (0b11 << 6);
+        break;
+    default:
+        assert(argChannelAMode == OutputCompareMode::DISCONNECTED || argChannelAMode == OutputCompareMode::TOGGLE || argChannelAMode == OutputCompareMode::NON_INVERTING_PWM || argChannelAMode == OutputCompareMode::INVERTING_PWM);
+        break;
+    }
+    mUniversalRegisters.tccra.write(temp);
+    mChannelAMode = argChannelAMode;
+}
+
+void Timer8::setChannelBMode(OutputCompareMode argChannelBMode)
+{
+    uint8_t temp = mUniversalRegisters.tccra.read();
+    temp &= ~(0b11 << 4);
+    switch (argChannelBMode)
+    {
+    case OutputCompareMode::DISCONNECTED:
+        break;
+    case OutputCompareMode::TOGGLE:
+        temp |= (1 << 4);
+        break;
+    case OutputCompareMode::NON_INVERTING_PWM:
+        temp |= (1 << 5);
+        break;
+    case OutputCompareMode::INVERTING_PWM:
+        temp |= (0b11 << 4);
+        break;
+    default:
+        assert(argChannelBMode == OutputCompareMode::DISCONNECTED || argChannelBMode == OutputCompareMode::TOGGLE || argChannelBMode == OutputCompareMode::NON_INVERTING_PWM || argChannelBMode == OutputCompareMode::INVERTING_PWM);
+        break;
+    }
+    mUniversalRegisters.tccra.write(temp);
+    mChannelBMode = argChannelBMode;
+}
+
+/*
+Compare Output Mode (COM) Bits - TCCRnA
+
+Channel A:
+COMnA1 COMnA0
+
+00 = Normal port operation (OCnA disconnected)
+01 = Toggle OCnA on compare match*
+10 = Non-inverting PWM
+11 = Inverting PWM
+
+Channel B:
+COMnB1 COMnB0
+
+00 = Normal port operation (OCnB disconnected)
+01 = Toggle OCnB on compare match*
+10 = Non-inverting PWM
+11 = Inverting PWM
+
+* Toggle mode is only available in certain timer modes (refer to the
+  datasheet for mode-specific behavior). It is not a general PWM mode.
+*/
--- lib/Timer8.h
+++ lib/Timer8.h
@@ -12,9 +12,9 @@ struct UniversalTimerRegisters
 
 struct Timer8SpecificRegisters
 {
-    Register8 *tcnt;
-    Register8 *ocra;
-    Register8 *ocrb;
+    Register8 &tcnt;
+    Register8 &ocra;
+    Register8 &ocrb;
 };
 
 class Timer8
@@ -37,27 +37,25 @@ public:
         PHASE_CORRECT_PWM,
         PHASE_CORRECT_PWM_COMPARE_A_TOP
     };
+    enum class OutputCompareMode {
+        DISCONNECTED,
+        TOGGLE,
+        NON_INVERTING_PWM,
+        INVERTING_PWM
+    };
     Timer8(UniversalTimerRegisters argUniversalTimerRegisters, Timer8SpecificRegisters argTimer8SpecificRegisters);
+    bool isRunning();
     void setPrescaler(Prescaler argPrescalar);
     void setMode(Mode argMode);
-    void setCompareValueA(int argCompareValue);
-    int getCompareValueA();
-    void setCompareValueB(int argCompareValue);
-    void getCompareValueB();
-    void setCompareATop(bool argCompareAIsTop);
+    void writeCompareValueA(uint8_t argCompareValue);
+    void writeCompareValueB(uint8_t argCompareValue);
     void start();
     void stop();
-    void enableChannelA();
-    void disableChannelA();
-    void setChannelADutyCycle();
-    void enableChannelB();
-    void disableChannelB();
-    void setChannelBDutyCycle();
+    void setChannelAMode(OutputCompareMode argChannelAMode);
+    void setChannelBMode(OutputCompareMode argChannelBMode);
 
 private:
     void writePrescaler();
-    void writeCompareValueA();
-    void writeCompareValueB();
     void enableCTC();
     void enableFastPWM();
     void enableFastPWMCompareATop();
@@ -67,11 +65,6 @@ private:
     Timer8SpecificRegisters mTimer8SpecificRegisters;
     Prescaler mPrescaler = Prescaler::DIV_1;
     Mode mMode = Mode::NORMAL;
-    bool mIsOn = false;
-    int mCompareValueA = 255;
-    int mCompareValueB = 255;
-    bool mChannelAIsOn = false;
-    bool mChannelBIsOn = false;
-    uint8_t mChannelADutyCycle;
-    uint8_t mChannelBDutyCycle;
+    OutputCompareMode mChannelAMode = OutputCompareMode::DISCONNECTED;
+    OutputCompareMode mChannelBMode = OutputCompareMode::DISCONNECTED;
 };
